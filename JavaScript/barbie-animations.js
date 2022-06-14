1ec7d1bcddb7837aba39a0e89d9bfffa159b2c57
Rudzikowa <eaadoru@students.eaaa.dk>
2022-06-15 00:13:54 +0200

    barbies and some not workung code

--- JavaScript/barbie-animations.js
+++ JavaScript/barbie-animations.js
@@ -1,18 +1,21 @@
 var playBtn = document.getElementById('play'),
   resetBtn = document.getElementById('reset'),
   hearbeat = document.getElementById('heartbeat')
-	audios = document.querySelectorAll('audio');
+audios = document.querySelectorAll('audio');
 console.log(audios);
 
 
-playBtn.addEventListener('mouseover', function() {
-[].forEach.call(audios, function(audio) {
-  // do whatever
-  audio.play();
-});
+playBtn.addEventListener('mouseover', function () {
+  [].forEach.call(audios, function (audio) {
+    // do whatever
+    audio.play();
+  });
+  document.getElementById(barbies).style.transition = "2s";
+  document.getElementById(barbies).style.left = "100px";
+  document.getElementById(barbies).style.bottom = "245px";
 }, false);
 
-playBtn.addEventListener('mouseleave', function() {
+playBtn.addEventListener('mouseleave', function () {
   heartbeat.pause();
   heartbeat.currentTime = 0;
 }, false);
