011300517ed2e834ff93f72a544976a971dda811
Giulia Ivan <eaagril@students.eaaa.dk>
2022-06-22 21:03:51 +0200

    playlist comments

--- JavaScript/playlists.js
+++ JavaScript/playlists.js
@@ -7,32 +7,22 @@ const sunVinyl = document.getElementById("sun-vinyl");
 const daySpotify = document.getElementById("day-spotify");
 
 // Create a function that shows/hides the Spotify playlist if you click on the Vinyl image
+// When you click on the Sun Vinyl
 sunVinyl.onclick = function () {
   // If the Spotify playlist isn't shown
   if (daySpotify.style.display !== "block") {
     // then show it
     daySpotify.style.display = "block";
-
+    // and change the background of the page from the small one to the extended one
     document.body.style.backgroundImage = "url(/Pictures/backgrounds/desktop/playlist2_pc.png)";
 
-    // moonVinyl.classList.add("margin-top-extended-bgk");
-
-  } else if (daySpotify.style.display !== "none" && nightSpotify.style.display !== "none") {
+  } else {
+    // otherwise, if the Spotify playlist is already shown before you clicked on the Sun Vinyl
+    // then when you click on it
+    // it will hide the Spotify playlist
     daySpotify.style.display = "none";
-    // then hide it
-
+    // and change the background from the extended one to the small one
     document.body.style.backgroundImage = "url(/Pictures/backgrounds/desktop/playlist-small_pc.png)";
-
-    // moonVinyl.classList.remove("margin-top-extended-bgk");
-  }
-
-  else {
-    document.body.style.backgroundImage = "url(/Pictures/backgrounds/desktop/playlist-small_pc.png)";
-
-    nightSpotify.style.display = "none";
-    daySpotify.style.display = "none";
-
-    // moonVinyl.classList.remove("margin-top-extended-bgk");
   }
 };
 
@@ -43,14 +33,15 @@ const moonVinyl = document.getElementById("moon-vinyl");
 const nightSpotify = document.getElementById("night-spotify");
 
 // Create a function that shows/hides the Spotify playlist if you click on the Vinyl image
+// When you click on the Moon Vinyl
 moonVinyl.onclick = function () {
   // If the Spotify playlist isn't shown
   if (nightSpotify.style.display !== "block") {
     // then show it
     nightSpotify.style.display = "block";
   } else {  //if it is shown
-    nightSpotify.style.display = "none";
     // then hide it
+    nightSpotify.style.display = "none";
   }
 };
 
