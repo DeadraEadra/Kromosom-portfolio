23e499b7403bbeb76ef534e4e55556ea6f00f1fb
Giulia Ivan <eaagril@students.eaaa.dk>
2022-06-22 21:57:16 +0200

    barbie-animation js comments

--- JavaScript/barbie-animations.js
+++ JavaScript/barbie-animations.js
@@ -1,16 +1,21 @@
 // PLAY SOUND WHEN MOUSEOVER AND MOUSELEAVE THE BARBIE IMAGE
 
-//Assigning the names
+//Assigning the names ( declaring the variables for play and reset button, and also the audio file )
 var playBtn = document.getElementById('play'),
   resetBtn = document.getElementById('reset'),
   hearbeat = document.getElementById('heartbeat')
+  // the id of the audio we will play afterwards using an EventListener
+
 audios = document.querySelectorAll('audio');
+// returns a list of the document's elements that match the group of selectors named "audio"
 console.log(audios);
+// inform us if the list was returned
 
 
 //When the mouse is over, the audio will be played
 playBtn.addEventListener('mouseover', function () {
-  [].forEach.call(audios, function (audio) {
+  [].forEach.call(audios, function (audio) { //every time the function is called = when the mouse is over the img
+    // then execute the function "audio" which plays the audio
     audio.play();
   });
 }, false);
